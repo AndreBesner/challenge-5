85b5d821e18406875b127c2ed3278fa8b2f66b56
Andre Besner <besner.andre@gmail.com>
2023-04-06 15:16:05 -0400

    get printing back of local storage, mainly 9am work, code clean, removal of debugging tools

--- Develop/script.js
+++ Develop/script.js
@@ -18,26 +18,15 @@ let thisTime = dayjs().format('MMMM DD, YYYY')
     }
   })
   }, 1000)
-
   $(".saveBtn").click(function(){
-    console.log("you clicked a save button");
     let idHour = $(this).parents().attr("data-time");
-    console.log("test" +idHour);
-    console.log($(this).parents().attr("data-time"));
     let text = $(this).siblings("textarea").val();
-    console.log(text);
     localStorage.setItem(idHour, text);
-    console.log(localStorage.getItem("09"));
   })
-
   function displayDescriptions(){
     for(var i = 9 ; i <= 17 ; i++){
-        var desc = localStorage.getItem(i);
-        console.log(desc);
-        $(".time-block[data-time='" + i + "'] .description").val(desc);
-        if (i == 9 && desc != null) {
-          $(".time-block[data-time='09'] .description").val(desc);
-        }
+        var desc = localStorage.getItem(i.toString().padStart(2, '0'));
+        $(".time-block[data-time='" + i.toString().padStart(2, '0') + "'] .description").val(desc);
     }
-}
+  }
 });
